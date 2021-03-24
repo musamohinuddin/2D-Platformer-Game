45f4d490dd27a999614575f63a22e726415f3206
K S M Musa Mohinuddin <musamohinuddin@gmail.com>
2021-03-24 14:25:48 +0530

    1) Added code to accept horizontal input from user. 2) Added code to play run animation and flip player accordingly to user input.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private void Awake()
+    public Animator animator;
+	private void Awake()
 	{
 		Debug.Log("Player controller awake");
 	}
 	
-	private void OnCollisionEnter2D(Collision2D collision)
-	{
-		Debug.Log("Collision: " + collision.gameObject.name);
-	}
-	
-	
-	
-	
-	
+	//private void OnCollisionEnter2D(Collision2D collision)
+	//{
+	//	Debug.Log("Collision: " + collision.gameObject.name);
+	//}
+		
 	
 	// Start is called before the first frame update
     void Start()
@@ -28,6 +25,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       float speed =Input.GetAxisRaw("Horizontal");
+		animator.SetFloat("Speed", Mathf.Abs(speed));
+		Vector3 Scale = transform.localScale;
+		if(speed < 0)
+		{
+			Scale.x = -1f * Mathf.Abs(Scale.x);
+		} else if(speed > 0)
+		{
+			Scale.x = Mathf.Abs(Scale.x);
+		}
+		transform.localScale=Scale;
+			
+			
     }
 }
