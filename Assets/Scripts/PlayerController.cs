b2add49640fb29bd00db7a47b804157278c878fc
K S M Musa Mohinuddin <musamohinuddin@gmail.com>
2021-03-24 13:47:12 +0530

    Created PlayerController script, added RigidBody and Collider. Also detected the collision between Player and Platform.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void Awake()
+	{
+		Debug.Log("Player controller awake");
+	}
+	
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		Debug.Log("Collision: " + collision.gameObject.name);
+	}
+	
+	
+	
+	
+	
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
