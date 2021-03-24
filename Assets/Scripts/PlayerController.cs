e87799ab9d0fed04f8e7b2f2a747d765abb5e713
K S M Musa Mohinuddin <musamohinuddin@gmail.com>
2021-03-24 18:10:20 +0530

    1) Made crouch animation on pressing left Control. 2) Made Jump by taking Vertical input.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,10 @@ public class PlayerController : MonoBehaviour
 		Debug.Log("Player controller awake");
 	}
 	
+	
+	  
+     
+	
 	//private void OnCollisionEnter2D(Collision2D collision)
 	//{
 	//	Debug.Log("Collision: " + collision.gameObject.name);
@@ -25,7 +29,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       float speed =Input.GetAxisRaw("Horizontal");
+		float speed =Input.GetAxisRaw("Horizontal");
 		animator.SetFloat("Speed", Mathf.Abs(speed));
 		Vector3 Scale = transform.localScale;
 		if(speed < 0)
@@ -37,6 +41,26 @@ public class PlayerController : MonoBehaviour
 		}
 		transform.localScale=Scale;
 			
-			
+		//Code to make Crouch animation
+		if(Input.GetKeyDown(KeyCode.LeftControl))
+		{
+			 //crouch = true;
+             animator.SetBool("Crouch", true);
+		} else if(Input.GetKeyUp(KeyCode.LeftControl)) 
+		{
+			//crouch = false;
+             animator.SetBool("Crouch", false);
+		}
+	
+		//Code to make Jump animation
+		float jump =Input.GetAxisRaw("Vertical");
+		if(jump > 0) 
+		{
+			animator.SetBool("Jump", true);
+		} else if(jump < 0) 
+		{
+			animator.SetBool("Jump", false);
+		}
+		
     }
 }
