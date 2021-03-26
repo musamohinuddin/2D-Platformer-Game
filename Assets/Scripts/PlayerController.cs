066133a7678d1c3789ec1708ff4af58aa8d07f63
K S M Musa Mohinuddin <musamohinuddin@gmail.com>
2021-03-26 23:17:54 +0530

    Added Player Run Horizontal Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
+	public float speed;
 	private void Awake()
 	{
 		Debug.Log("Player controller awake");
@@ -26,41 +27,63 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-		float speed =Input.GetAxisRaw("Horizontal");
-		animator.SetFloat("Speed", Mathf.Abs(speed));
+	// Update is called once per frame
+	void Update()
+	{
+		//Code to make Run Animation
+		float horizontal = Input.GetAxisRaw("Horizontal");
+		MoveCharacter(horizontal);
+		PlayMovementAnimation(horizontal);
+	}
+
+	//Function to make Player Movement
+	private void MoveCharacter(float horizontal) 
+	{
+		Vector3 position = transform.position;
+		position.x += horizontal * speed * Time.deltaTime;
+		transform.position = position;
+	}
+
+	private void PlayMovementAnimation(float horizontal) 
+	{
+		animator.SetFloat("Speed", Mathf.Abs(horizontal));
 		Vector3 Scale = transform.localScale;
-		if(speed < 0)
+		if (horizontal < 0)
 		{
 			Scale.x = -1f * Mathf.Abs(Scale.x);
-		} else if(speed > 0)
+			
+		}
+		else if (horizontal > 0)
 		{
 			Scale.x = Mathf.Abs(Scale.x);
-		}
-		transform.localScale=Scale;
 			
-		//Code to make Crouch animation
-		if(Input.GetKeyDown(KeyCode.LeftControl))
-		{
-			 //crouch = true;
-             animator.SetBool("Crouch", true);
-		} else if(Input.GetKeyUp(KeyCode.LeftControl)) 
-		{
-			//crouch = false;
-             animator.SetBool("Crouch", false);
 		}
-	
+		transform.localScale = Scale;
+
+		//Code to make Crouch animation
+		if (Input.GetKeyDown(KeyCode.LeftControl))
+		{
+
+			animator.SetBool("Crouch", true);
+		}
+		else if (Input.GetKeyUp(KeyCode.LeftControl))
+		{
+
+			animator.SetBool("Crouch", false);
+		}
+
 		//Code to make Jump animation
-		float jump =Input.GetAxisRaw("Vertical");
-		if(jump > 0) 
+		float jump = Input.GetAxisRaw("Vertical");
+		if (jump > 0)
 		{
 			animator.SetBool("Jump", true);
-		} else if(jump < 0) 
+		}
+		else if (jump < 0)
 		{
 			animator.SetBool("Jump", false);
 		}
+	}
 		
-    }
+		
+   
 }
