efe265ef3c67c2ab71ef4d5952798ad2839a7209
musamohinuddin <musamohinuddin@gmail.com>
2021-04-17 09:06:05 +0530

    Added LevelManager as a Singleton and locked all the level. Completed Chapter 10- Mini Assignment1 - Lock Unlock.

---
+++ Assets/Scripts/Levels/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    private static LevelManager instance;
+    public static LevelManager Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public LevelStatus GetLevelStatus(string Level)
+    {
+        LevelStatus levelStatus = (LevelStatus)PlayerPrefs.GetInt(Level, 0);
+        return levelStatus;
+    }
+
+    public void SetLevelStatus(string Level, LevelStatus levelStatus)
+    {
+        PlayerPrefs.SetInt(Level, (int)levelStatus);
+    }
+}
