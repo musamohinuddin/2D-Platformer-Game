6fc3f200dfbf8d5bdfc04e07604c6c6791c7be7e
musamohinuddin <musamohinuddin@gmail.com>
2021-04-17 20:33:24 +0530

    Added 5 levels and also added Level Complete screen and made next level Unlock. Completed Chapter10 Challenge - Bonus.

--- Assets/Scripts/Levels/LevelManager.cs
+++ Assets/Scripts/Levels/LevelManager.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
 
 public class LevelManager : MonoBehaviour
 {
     private static LevelManager instance;
     public static LevelManager Instance { get { return instance; } }
+
+    public string[] Levels;
 
     private void Awake()
     {
@@ -16,6 +20,34 @@ public class LevelManager : MonoBehaviour
         } else
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        if(GetLevelStatus(Levels[0]) == LevelStatus.Locked)
+        {
+            SetLevelStatus(Levels[0], LevelStatus.Unlocked);
+        }
+    }
+
+    public void MarkCurrentLevelComplete()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        //set level status to complete
+        SetLevelStatus(currentScene.name, LevelStatus.Completed);
+
+        //Unlock next level
+        int currentSceneIndex = Array.FindIndex(Levels, Levels => Levels == currentScene.name);
+        int nextSceneIndex = currentSceneIndex + 1;
+        if(nextSceneIndex < Levels.Length)
+        {
+            SetLevelStatus(Levels[nextSceneIndex], LevelStatus.Unlocked);
+            SceneManager.LoadScene(6);
+        } else
+        {
+            Debug.Log("Congrats you have completed all levels.");
         }
     }
 
